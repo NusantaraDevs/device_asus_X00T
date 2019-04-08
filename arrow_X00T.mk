42a0bcf78c29980d186174f3d740cc607b041029
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    X00T: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Signed-off-by: Najahiiii <najahiii@outlook.co.id>

--- arrow_X00T.mk
+++ arrow_X00T.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit some common ArrowOS stuff
 $(call inherit-product, vendor/arrow/config/common.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
