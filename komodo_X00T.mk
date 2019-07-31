e7634fce7c232ef02e978aab1d537b738bd0f8c1
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-07-31 13:15:32 +0700

    X00T: update fingerprint from 16.2017.1906.056 firmware
    * Add back some system and vendor init prop too
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- komodo_X00T.mk
+++ komodo_X00T.mk
@@ -42,7 +42,18 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1181 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1261 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1903.050-20190401:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1906.056-20190722:user/release-keys
+
+# The following system and vendor props will be set by vendor init
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.device \
+    ro.product.model \
+    ro.product.name
+
+PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
+    ro.vendor.product.device \
+    ro.vendor.product.model \
+    ro.vendor.product.name
