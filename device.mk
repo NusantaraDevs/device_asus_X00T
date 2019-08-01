9ceedd3ccf739e8ec225bb8e5048cefb5768f81c
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-08-01 07:13:43 +0700

    X00T: re-add G711 Encoder, partially revert commit 4a8724be
    commit 4a8724be ("X00T: remove unused media packages") remove unused
    G711 encoder, though everything was fine, since this is included in
    stock rom, let's build it as well.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- device.mk
+++ device.mk
@@ -282,13 +282,13 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libaacwrapper \
     libc2dcolorconvert \
     libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
     libOmxEvrcEnc \
+    libOmxG711Enc \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
@@ -454,6 +454,7 @@ PRODUCT_PACKAGES += \
 
 # WFD
 PRODUCT_PACKAGES += \
+    libaacwrapper \
     libnl
 
 PRODUCT_BOOT_JARS += \
