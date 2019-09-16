0ecc9fd86eead1dd7285afce5e91c0eff00df25b
Najahiiii <najahiii@outlook.co.id>
2019-09-16 08:45:34 +0700

    X00T: Temporary disable sepolicy for Q
    Signed-off-by: Najahiiii <najahiii@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,8 +238,8 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2019-06-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
