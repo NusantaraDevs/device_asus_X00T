2dbe38b7ba0fdebaccb2d47eb3074ecb5d2b1860
Najahiiii <najahiii@outlook.co.id>
2019-09-17 00:45:16 +0700

    X00T: Don't build Trust HAL
    This reverts commit 2733f1986d3c0d2dc4a17548e79ed51930e98e7e.

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 # 
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -440,10 +440,6 @@ PRODUCT_COPY_FILES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
